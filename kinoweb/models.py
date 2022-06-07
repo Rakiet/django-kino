5bd63b916ea6f7b8e2aa149509e860db35ad16bc
Piotr Żakieta <piter5331@gmail.com>
2022-06-07 15:33:17 +0200

    edit move model

--- kinoweb/models.py
+++ kinoweb/models.py
@@ -4,4 +4,13 @@ from django.db import models
 class Move(models.Model):
     title = models.CharField(max_length=64, blank=False)
     year = models.PositiveSmallIntegerField(default=2000)
+    description = models.TextField(default="")
+    premiere = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    poster = models.ImageField(upload_to="posters", null=True, blank=True)
 
+    def __str__(self):
+        return self.titleWithYear()
+
+    def titleWithYear(self):
+        return "{} ({})".format(self.title, self.year)
