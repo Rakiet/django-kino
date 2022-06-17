b248f475f6e589a7822966de6075de8f19376eeb
Piotr Żakieta <piter5331@gmail.com>
2022-06-17 18:22:30 +0200

    registered new model RatingMovie and migrate

--- kinoweb/admin.py
+++ kinoweb/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Move, ExtraInfoToMovie
+from .models import Move, ExtraInfoToMovie, RatingMovie
 # Register your models here.
 
 admin.site.register(Move)
 admin.site.register(ExtraInfoToMovie)
+admin.site.register(RatingMovie)
