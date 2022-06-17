320f3741065eea79c516792819a1a477d097f979
Piotr Żakieta <piter5331@gmail.com>
2022-06-17 20:14:38 +0200

    create url film/<int:id>

--- kinoweb/urls.py
+++ kinoweb/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from kinoweb.views import allMovies, newMovie, editMovie, removeMovie
+from kinoweb.views import allMovies, newMovie, editMovie, removeMovie, singleMovie
 
 urlpatterns = [
     path('wszystkie-filmy/', allMovies, name="allMovies"),
     path('nowy-film/', newMovie, name="newMovie"),
     path('edytuj-film/<int:id>/', editMovie, name="editMovie"),
     path('usun-film/<int:id>/', removeMovie, name="removeMovie"),
+    path('film/<int:id>/', singleMovie, name="singleMovie"),
 ]
