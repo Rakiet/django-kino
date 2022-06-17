c72f52a7d1ba3cbdf238425303cb31a0e7b70dd2
Piotr Żakieta <piter5331@gmail.com>
2022-06-17 20:13:39 +0200

    created forms to add new coments

--- kinoweb/forms.py
+++ kinoweb/forms.py
@@ -1,8 +1,13 @@
 from django.forms import  ModelForm
-from .models import Move
+from .models import Move, RatingMovie
 
 class MoveForm(ModelForm):
     class Meta:
         model = Move
         fields = ['title', 'year', 'description', 'premiere', 'imdb_rating', 'poster']
+
+class RatingForm(ModelForm):
+    class Meta:
+        model = RatingMovie
+        fields = ['textReview', 'stars']
 
