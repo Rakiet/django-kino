3de4f6c3b294c078b8470c09696aeb358592230b
Piotr Żakieta <piter5331@gmail.com>
2022-06-17 18:12:55 +0200

    add migrate ExtraInfoToMovie and register

--- kinoweb/admin.py
+++ kinoweb/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Move
+from .models import Move, ExtraInfoToMovie
 # Register your models here.
 
 admin.site.register(Move)
+admin.site.register(ExtraInfoToMovie)
